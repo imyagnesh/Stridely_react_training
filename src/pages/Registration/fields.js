ddebc7febfe7fddc0d58df03ee8e65d54ca03670
aashkamehta99 <84374950+aashkamehta99@users.noreply.github.com>
2021-05-18 13:32:39 +0530

    fields

---
+++ src/pages/Registration/fields.js
@@ -0,0 +1,63 @@
+import TextInput from 'components/TextInput';
+export const fields = [
+    {
+        name: 'identifier',
+        initialValue: '',
+        component: TextInput,
+        label: 'Username',
+        validate: value => {
+          if (!value) {
+            return 'required';
+          }
+          return '';
+        },
+      },
+      {
+        name: 'id',
+        initialValue: '',
+        component: TextInput,
+        label: 'Lastname',
+        validate: value => {
+          if (!value) {
+            return 'required';
+          }
+          return '';
+        },
+      },
+      {
+        name: 'password',
+        initialValue: '',
+        component: TextInput,
+        label: 'Password',
+        type: 'password',
+        validate: value => {
+          if(!value)
+          {
+                return 'required'
+          }
+          if (document.getElementsByName('confirmPassword')[0].value!==value) {
+            
+            return 'password not match with confirm password';
+          }
+          return '';
+        },
+      },
+      {
+        name: 'confirmPassword',
+        initialValue: '',
+        component: TextInput,
+        label: 'ConfirmPassword',
+        type: 'password',
+        validate: value => {
+            if (document.getElementsByName('password')[0].value!==value) {
+                return 'password not match with confirm password';
+            }
+            if (!value) {
+            return 'required';
+          }
+          return '';
+        },
+      },
+];
+
+export const initialValues = fields.reduce((p, c) => ({ ...p, [c.name]: c.initialValue }), {});
